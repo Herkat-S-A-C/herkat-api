29366bcab3449fa006d78f9a2ac632d436b165c7
Braighton Paico <braightonfoulman@gmail.com>
2025-08-12 23:49:28 -0500

    fixing ServiceItemType controller

--- src/main/java/com/herkat/controllers/ServiceItemTypeController.java
+++ src/main/java/com/herkat/controllers/ServiceItemTypeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/service-item-type")
+@RequestMapping("/api/v1/service-item-types")
 public class ServiceItemTypeController {
 
     private final ServiceItemTypeService service;
