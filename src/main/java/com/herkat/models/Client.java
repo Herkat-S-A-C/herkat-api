0f88511aa1b88754bb43c1bb5cc6e9f317b16fad
Deyvi <deyviimanol@gmail.com>
2025-09-12 10:48:12 -0500

    Feature/client entity (#7)
    * Create Client class
    
    * Create Client class
    
    * Client class space correction

---
+++ src/main/java/com/herkat/models/Client.java
@@ -0,0 +1,48 @@
+package com.herkat.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(length = 100)
+    private String address;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {createdAt = LocalDateTime.now();
+    }
+    public static Client newClient(String name, String email, String phone, String address, LocalDateTime createdAt) {
+        return new Client(
+                null,
+                name,
+                email,
+                phone,
+                address,
+                createdAt);
+    }
+}
