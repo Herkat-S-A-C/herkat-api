30ccbbb4c09212d9aad05c3099edf3e5036b638c
Deyvi <deyviimanol@gmail.com>
2025-09-17 10:26:25 -0500

    Add DTO for the client (NewClientDto, UpdateClientDto, ClientDto

--- src/main/java/com/herkat/dtos/client/ClientDto.java
+++ src/main/java/com/herkat/dtos/client/ClientDto.java
@@ -1,4 +1,33 @@
 package com.herkat.dtos.client;
 
+import com.herkat.models.Client;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ClientDto {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    public static ClientDto fromEntity(Client client) {
+        return new ClientDto(
+                client.getId(),
+                client.getName(),
+                client.getEmail(),
+                client.getPhone(),
+                client.getAddress()
+        );
+    }
+
 }
