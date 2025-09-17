7752b6ee52949b406bbbed77318d5f265b5d4182
Deyvi <deyviimanol@gmail.com>
2025-09-17 11:05:07 -0500

    Feature/client-dto (#13)
    * Add DTO for the client (NewClientDto, UpdateClientDto, ClientDto
    
    * Add DTO for the client (NewClientDto, UpdateClientDto, ClientDto

---
+++ src/main/java/com/herkat/dtos/client/NewClientDto.java
@@ -0,0 +1,42 @@
+package com.herkat.dtos.client;
+
+import com.herkat.models.Client;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewClientDto {
+
+    @NotBlank(message = "El nombre del cliente es obligatorio.")
+    @Size(max = 50, message = "El nombre no puede superar los 50 caracteres.")
+    private String name;
+
+    @NotBlank(message = "El email del cliente es obligatorio.")
+    @Email(message = "El email debe de ser correcto.")
+    private String email;
+
+    @NotNull(message = "El numero del cliente es obligatorio.")
+    @Size(max = 20, message = "El numero no puede superar los 20 caracteres.")
+    private String phone;
+
+    @NotNull(message = "La direccion del cliente es obligatorio.")
+    @Size(max = 100, message = "La direccion no puede superar los 50 caracteres.")
+    private String address;
+
+    public static Client toEntity(NewClientDto newClientDto) {
+        return  Client.newClient(
+                newClientDto.getName(),
+                newClientDto.getEmail(),
+                newClientDto.getPhone(),
+                newClientDto.getAddress(),
+                null
+        );
+    }
+}
---
+++ src/main/java/com/herkat/dtos/client/UpdateClientDto.java
@@ -0,0 +1,35 @@
+package com.herkat.dtos.client;
+
+import com.herkat.models.Client;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateClientDto {
+
+    @Size(max = 50, message = "El nombre del cliente no puede superar los 50 caracteres.")
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    public static Client updateEntity(UpdateClientDto dto,
+                                      Client existingClient) {
+        return new Client(
+                existingClient.getId(),
+                dto.getName() != null && !dto.getName().isEmpty() ? dto.getName() : existingClient.getName(),
+                dto.getEmail() != null ? dto.getEmail() : existingClient.getEmail(),
+                dto.getPhone() != null && !dto.getPhone().isEmpty() ? dto.getPhone() : existingClient.getPhone(),
+                dto.getAddress() != null ? dto.getAddress() : existingClient.getAddress(),
+                existingClient.getCreatedAt()
+        );
+    }
+
+}
