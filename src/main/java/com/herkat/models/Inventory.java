14ae45e51962ccd39e6fe5350b8bbe39c6cb6a59
Piero Sensualidad <pieropamorivas@gmail.com>
2025-09-17 17:05:54 -0500

    feature/inventory-entity (#14)
    * Create Client class
    
    * Create Client class
    
    * Client class space correction
    
    * Create Inventory Class
    
    * Inventory class space correction
    
    * Revert "Feature/inventory entity (#8)" (#9)
    
    This reverts commit b166bff0df6c87fbff2bc02ec289a5e2074af908.
    
    * creation of the inventory class
    
    * Changes made and class ended
    
    * Changes made and class ended
    
    ---------
    
    Co-authored-by: Deyvi <deyviimanol@gmail.com>
    Co-authored-by: Braighton Paico <braightonfoulman@gmail.com>
    Co-authored-by: Piero Pamo <pieropamorivas@gmail.comgit config --global user.name Pierooooo-dev>

---
+++ src/main/java/com/herkat/models/Inventory.java
@@ -0,0 +1,38 @@
+package com.herkat.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   //ID autogenerado
+    private Integer id;
+
+    @Column(nullable = false)   //los datos ingresados no pueden ser nulos
+    private Integer stock;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;   //fecha y hora de ultima actualizacion
+
+    @PrePersist
+    protected void prePersist(){                 //actualizar día
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public static Inventory newInventory(Integer stock, LocalDateTime updatedAt){  //inicializar nuevo registro de inventario
+        return new Inventory(
+                null,
+                   stock,
+                   updatedAt);
+    }
+}
