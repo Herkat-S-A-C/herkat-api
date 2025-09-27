b05c23103bb0ade6a05ac7e2aec28f63411c03a8
Braighton Paico <braightonfoulman@gmail.com>
2025-09-27 14:14:24 -0500

    Feature/custom error handler (#33)
    * adding new custom error handlers
    
    * removing unnecessary imports

--- src/main/java/com/herkat/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/herkat/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,10 @@
 package com.herkat.exceptions;
 
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
